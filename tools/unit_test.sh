59169ee61da40ab266f0c0f90176b644dc24e464
Tamal Saha <tamal@appscode.com>
2020-06-22 10:00:17 -0700

    Enable GitHub Actions (#3)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- tools/unit_test.sh
+++ tools/unit_test.sh
@@ -5,5 +5,5 @@ set -ex
 go test -covermode atomic -coverprofile coverage.txt
 
 if [ -n "$CODECOV_TOKEN" ]; then
-  curl -s https://codecov.io/bash | bash
+    curl -s https://codecov.io/bash | bash
 fi
